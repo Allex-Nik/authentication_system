c6f4701a853cfef58402a8181789977451ec5faa
Allex-Nik <alexandr.nikolaev96@gmail.com>
2024-10-14 20:05:30 +0200

    Env vars added, password and secret token changed

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-cors-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-//    implementation("org.jetbrains.kotlinx:kotlinx-serialization-java-time")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
@@ -47,8 +46,6 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
-//    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-
     implementation("org.flywaydb:flyway-core:9.22.3")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
